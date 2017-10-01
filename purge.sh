734aae78293801bdce29cad20f69f4240a013dbb
whw3 <whw3@users.noreply.github.com>
2017-10-01 13:52:46 -0500

    add function force_purge

--- purge.sh
+++ purge.sh
@@ -2,11 +2,15 @@
 clear
 purge="docker rmi "
 force=0
+function force_purge()
+{
+    purge+="-f "
+    force=1
+    echo "Forcing purge"
+}
 while getopts ":f" opt; do
   case ${opt} in
-    f ) purge+="-f "
-        force=1
-        echo "Forcing purge"
+    f ) force_purge
       ;;
     \? ) echo "Usage: purge [-f]"
         exit 1;
@@ -14,7 +18,7 @@ while getopts ":f" opt; do
   esac
 done
 
-if [[ "$(docker ps -a -f "status=exited"| wc -l)" -gt 1 ]]; then 
+if [[ "$(docker ps -a -f "status=exited"| wc -l)" -gt 1 ]]; then
     if (whiptail --title "Stopped Containers Detected" --yesno "Should stopped containers be removed?" 8 78) then
         images=($(docker ps -a -f "status=exited" | grep -v "IMAGE" | awk '{print $1" "$2" on"}'))
         _list=$(whiptail --title "Remove Stopped Containers" --checklist --separate-output \
@@ -23,9 +27,7 @@ if [[ "$(docker ps -a -f "status=exited"| wc -l)" -gt 1 ]]; then
     else
         if [ $force = 0 ]; then
             if (whiptail --title "Stopped Containers Detected" --yesno "Should I force purge instead?" 8 78) then
-                purge+="-f "
-                force=1
-                echo "Forcing purge"
+                force_purge
             fi
         fi
     fi
@@ -33,5 +35,7 @@ fi
 images=($(docker images | grep -v "IMAGE" |uniq -f3|awk '{print $3" "$1":"$2" off"}'))
 _list=$(whiptail --title "Purge Docker Images" --checklist --separate-output \
 "Select images to purge:" 34 80 20 "${images[@]}" 3>&1 1>&2 2>&3)|| echo "Purge Cancelled"
-[[ ! -z "${_list[@]}" ]] && $purge "${_list[@]}" && exit 0
-exit 1
+[[ -z "${_list[@]}" ]] && exit 1
+_list=($(echo "${_list[@]}"| tr '\n' ' '))
+$purge "${_list[@]}"
+exit 0
